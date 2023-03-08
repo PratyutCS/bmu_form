d2a3b91cf63ae9f612a4ccd4d952e9555cfd491b
Pratyut <pratyut987@gmail.com>
2023-03-08 08:02:59 +0530

    init

--- fill.js
+++ fill.js
@@ -1,3 +1,15 @@
+let post=document.querySelector(".post");
+let school=document.querySelector(".school");
+let fname=document.querySelector(".fname");
+let mname=document.querySelector(".mname");
+let lname=document.querySelector(".lname");
+let name;
 function lol(){
-    alert(document.querySelector(".name").value);
+    if((mname.value).trim()===""){
+        name=(fname.value).trim()+" "+(lname.value).trim();
+    }
+    else{
+        name=(fname.value).trim()+" "+(mname.value).trim()+" "+(lname.value).trim();
+    }
+    console.log((post.value).trim()+" "+(school.value).trim()+" "+name);
 }
