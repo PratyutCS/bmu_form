3bb82abdf84eec63aa460e4e1ef998e69108173d
Pratyut <pratyut987@gmail.com>
2023-03-08 13:48:06 +0530

    init

---
+++ server.js
@@ -0,0 +1,38 @@
+let Express = require('express')
+let path = require('path')
+let bodyparser=require('body-parser');
+
+let app = new Express()
+
+app.use(bodyparser.urlencoded({extended: true}));
+
+let htmlfolder = path.join(__dirname, "public/html");
+let cssfolder = path.join(__dirname, "public/css");
+
+app.set('view engine','ejs');
+app.set("views",path.join(__dirname, "./templates/views"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(htmlfolder, "index.html"));
+})
+
+app.get("/style", (req, res) => {
+    res.sendFile(path.join(cssfolder, "style.css"));
+})
+
+app.post("/pdf", async(req, res) => {
+    const post= req.body.posted;
+    const school= req.body.school;
+    res.render('pdf',{post:post,
+                      school:school});
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(htmlfolder, "error.html"));
+})
+
+const PORT= process.env.PORT || 80
+
+app.listen(PORT, () => {
+    console.log("Server is running at lolz "+ PORT);
+})
